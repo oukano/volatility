3a6fc91d74cf74bf76ca421c861ea886e88ae864
oussama bouroukba <oussama.bouroukba@gmail.com>
2024-10-10 11:25:30 +0100

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,27 @@
 import streamlit as st
+import yfinance as yf
+from datetime import datetime, timedelta
+import pytz
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+# Define available ticker symbols and their names
+ticker_options = {
+    "QQQ": "Nasdaq",
+    "GLD": "XAU",
+    "SPY": "S&P"
+}
+
+st.title("CTT - Expected Move Calculator")
+
+# Create a dropdown for selecting the ticker symbol
+selected_ticker = st.selectbox("Select Ticker Symbol", options=list(ticker_options.keys()), format_func=lambda x: ticker_options[x])
+st.write(f"Selected Ticker: {selected_ticker}")
+
+# Fetch the ticker data
+ticker = yf.Ticker(selected_ticker)
+print(ticker)
+
+for i in range(1, 4):
+    yesterday_close = fetch_stock_data(ticker, days_back=i)
+    if yesterday_close:
+        print("yesterday_close : " + yesterday_close)
+        break
